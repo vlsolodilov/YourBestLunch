65fe0dcf9d35ce05a3e5b190fe9c4a14e1a14909
vlsolodilov <vlsolodilov@gmail.com>
2021-12-27 22:49:23 +0300

    add scripts for db

---
+++ src/main/resources/db/initDB_hsql.sql
@@ -0,0 +1,54 @@
+DROP TABLE user_roles IF EXISTS;
+DROP TABLE vote IF EXISTS;
+DROP TABLE lunch_item IF EXISTS;
+DROP TABLE restaurant IF EXISTS;
+DROP TABLE users IF EXISTS;
+
+CREATE TABLE users
+(
+    id               INTEGER IDENTITY PRIMARY KEY,
+    name             VARCHAR(255)            NOT NULL,
+    email            VARCHAR(255)            NOT NULL,
+    password         VARCHAR(255)            NOT NULL,
+    registered       TIMESTAMP DEFAULT now() NOT NULL,
+    enabled          BOOLEAN   DEFAULT TRUE  NOT NULL
+);
+CREATE UNIQUE INDEX users_unique_email_idx
+    ON USERS (email);
+
+CREATE TABLE user_roles
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR(255),
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES USERS (id) ON DELETE CASCADE
+);
+
+CREATE TABLE restaurant
+(
+    id      INTEGER IDENTITY PRIMARY KEY,
+    name    VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE lunch_item
+(
+    id            INTEGER IDENTITY PRIMARY KEY,
+    date_time     TIMESTAMP    NOT NULL,
+    name          VARCHAR(255) NOT NULL,
+    price         INTEGER      NOT NULL,
+    restaurant_id INTEGER      NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE
+);
+
+CREATE TABLE vote
+(
+    id            INTEGER IDENTITY PRIMARY KEY,
+    date_time     TIMESTAMP NOT NULL,
+    user_id       INTEGER   NOT NULL,
+    restaurant_id INTEGER   NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX vote_unique_user_datetime_idx
+    ON vote (user_id, date_time)
